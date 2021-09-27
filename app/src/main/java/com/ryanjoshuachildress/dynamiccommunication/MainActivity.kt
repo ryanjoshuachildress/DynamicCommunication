85593149b7e35e56675e76696f7706ecf402d806
ryanjoshuachildress <ryanjoshuachildress@gmail.com>
2021-09-27 09:23:26 -0700

    Firebase dependency

--- app/src/main/java/com/ryanjoshuachildress/dynamiccommunication/MainActivity.kt
+++ app/src/main/java/com/ryanjoshuachildress/dynamiccommunication/MainActivity.kt
@@ -2,10 +2,35 @@ package com.ryanjoshuachildress.dynamiccommunication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.ViewGroup
+import android.widget.Button
+import com.google.firebase.analytics.FirebaseAnalytics
+import com.google.firebase.analytics.ktx.analytics
+import com.google.firebase.ktx.Firebase
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var analytics: FirebaseAnalytics
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // Obtain the FirebaseAnalytics instance.
+        analytics = Firebase.analytics
+
+
+        // Creates a button that mimics a crash when pressed
+        val crashButton = Button(this)
+        crashButton.text = "Test Crash"
+        crashButton.setOnClickListener {
+            throw RuntimeException("Test Crash") // Force a crash
+        }
+
+        addContentView(crashButton, ViewGroup.LayoutParams(
+            ViewGroup.LayoutParams.MATCH_PARENT,
+            ViewGroup.LayoutParams.WRAP_CONTENT))
+
+
     }
 }
